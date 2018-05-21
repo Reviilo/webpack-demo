012804fed9404dca9c6cfc6b97de9caeaad8cab3
Revi <oliiv@outlook.com>
2018-05-21 08:41:53 -0300

    hot module replacement - middleware with express

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const app = express();
 const config = require('./webpack.config.js');
@@ -9,8 +10,11 @@ const webpackCompiler = webpack(config);
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
 app.use(webpackDevMiddleware(webpackCompiler, {
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
+  hot: true
 }));
+
+app.use(webpackHotMiddleware(webpackCompiler));
 
 // Serve the files on port 3000.
 const port = 3000;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'; 
 import printMe from './print.js';
+import './style.css';
 
 function component() {
   var element = document.createElement('dic');
@@ -16,4 +17,18 @@ function component() {
 }
 
 
-document.body.appendChild(component());
+// document.body.appendChild(component()); 
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
+
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the update printMe module!');
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
+  })
+} else {
+  console.log('warning!');
+}
